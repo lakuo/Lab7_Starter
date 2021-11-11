10a2b69de5d5244f6af70267e5b457e46ffbcde4
Laurence Kuo <lakuo@ucsd.edu>
2021-11-11 14:23:39 -0800

    update

--- assets/scripts/Router.js
+++ assets/scripts/Router.js
@@ -38,6 +38,7 @@ export class Router {
      * router instance using the 'this' keyword. Substitute 'home' for the variable
      * page
      */
+    this[page] = pageFunc;
   }
 
   /**
@@ -49,6 +50,15 @@ export class Router {
    */
   navigate(page, statePopped) {
     console.log(`navigate() function called, requested page: ${page}`);
+    if (!this[page]) {
+      console.log('Error: Function doesn\'t exist')
+    }
+    const hash = page == 'home' ? '' : '#' + page
+    if (!statePopped && window.location.hash != hash) {
+      history.pushState({ page: page }, '', window.location.origin + hash)
+    }
+    this[page]()
+
     /**
      * TODO - Part 1 - Step 4
      * Now, we are going to call the functions that we stored earlier based on 
